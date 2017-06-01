365e6195d64e525ef6f6e6bbb22b8fbf57b24df0
Martin Atkins <mart@degeneration.co.uk>
2017-05-31 18:09:57 -0700

    provider/nomad: begin to customize diff to include nomad plan info
    Nomad has its own plan functionality, so we can use the new CustomizeDiff
    functionality to integrate with that, allowing us to detect allocation
    errors during the plan phase and to ensure that we fail if something else
    changes the Nomad job outside of Terraform before we get to apply the
    plan.
    
    This is not a fully-complete change, and is more just a prototype of what
    this sort of thing might look like under this new API.

--- builtin/providers/nomad/resource_job.go
+++ builtin/providers/nomad/resource_job.go
@@ -3,11 +3,14 @@ package nomad
 import (
 	"bytes"
 	"encoding/gob"
+	"encoding/json"
 	"fmt"
 	"log"
 	"reflect"
+	"strconv"
 	"strings"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/hashicorp/nomad/api"
 	"github.com/hashicorp/nomad/jobspec"
 	"github.com/hashicorp/nomad/nomad/structs"
@@ -20,7 +23,8 @@ func resourceJob() *schema.Resource {
 		Update: resourceJobRegister,
 		Delete: resourceJobDeregister,
 		Read:   resourceJobRead,
-		Exists: resourceJobExists,
+
+		CustomizeDiff: resourceJobCustomizeDiff,
 
 		Schema: map[string]*schema.Schema{
 			"jobspec": {
@@ -37,14 +41,206 @@ func resourceJob() *schema.Resource {
 				Type:        schema.TypeBool,
 			},
 
-			"deregister_on_id_change": {
-				Description: "If true, the job will be deregistered when the job ID changes.",
-				Optional:    true,
-				Default:     true,
-				Type:        schema.TypeBool,
+			"id": {
+				Description: "Id of the job itself.",
+				Computed:    true,
+				Type:        schema.TypeString,
+			},
+
+			"name": {
+				Description: "Name of the job.",
+				Computed:    true,
+				Type:        schema.TypeString,
+			},
+
+			"evaluation_id": {
+				Description: "Id of the current evaluation for the job.",
+				Computed:    true,
+				Type:        schema.TypeString,
+			},
+
+			"job_modify_index": {
+				Description: "Incrementing number used to identify a particular modification of the job.",
+				Computed:    true,
+				Type:        schema.TypeString, // this is a uint64 in Nomad, so we can't use TypeInt
+			},
+
+			// Remaining computed fields are exposed primarily to expose the
+			// Nomad diff in the Terraform plan, but can also potentially
+			// be used for interpolations.
+			// At present this isn't fully comprehensive, just cherry-picking
+			// a few fields that are likely to be interesting. Fully
+			// representing the Nomad structure here would be nice, but
+			// is a non-trivial amount of work, so we'll consider this subset
+			// better than what we had before, which was nothing at all.
+
+			"region": {
+				Computed: true,
+				Type:     schema.TypeString,
+			},
+
+			"priority": {
+				Computed: true,
+				Type:     schema.TypeInt,
+			},
+
+			"datacenters": {
+				Computed: true,
+				Type:     schema.TypeList,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+
+			"task_groups": {
+				Computed: true,
+				Type:     schema.TypeList,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Description: "Name of the task group.",
+							Computed:    true,
+							Type:        schema.TypeString,
+						},
+						"count": {
+							Computed: true,
+							Type:     schema.TypeInt,
+						},
+						"meta": {
+							Computed: true,
+							Type:     schema.TypeMap,
+						},
+
+						"tasks": {
+							Computed: true,
+							Type:     schema.TypeList,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"name": {
+										Computed: true,
+										Type:     schema.TypeString,
+									},
+									"driver": {
+										Computed: true,
+										Type:     schema.TypeString,
+									},
+									"config_json": {
+										Computed: true,
+										Type:     schema.TypeString,
+									},
+								},
+							},
+						},
+					},
+				},
 			},
 		},
 	}
+}
+
+func resourceJobCustomizeDiff(d *schema.ResourceDiff, meta interface{}) error {
+	client := meta.(*api.Client)
+
+	if !d.HasChange("jobspec") {
+		// Nothing to do!
+		return nil
+	}
+
+	// Get the jobspec itself
+	jobspecRaw := d.Get("jobspec").(string)
+	jobspecAPI, err := prepareJobSpec(jobspecRaw)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[DEBUG] Generating plan for job")
+	resp, _, err := client.Jobs().Plan(jobspecAPI, true, nil)
+	if err != nil {
+		return fmt.Errorf("error from nomad plan: %s", err)
+	}
+
+	d.SetNew("id", jobspecAPI.ID)
+	d.SetNew("name", jobspecAPI.Name)
+
+	if d.HasChange("id") {
+		d.ForceNew("id")
+	}
+	if d.HasChange("name") {
+		d.ForceNew("name")
+	}
+
+	// Each change creates a new evaluation.
+	d.SetNewComputed("evaluation_id")
+
+	// After we apply, the job_modify_index will have been incremented, but
+	// we don't know by how much because registering a job can potentially
+	// make many small changes.
+	d.SetNewComputed("job_modify_index")
+
+	nomadDiff := resp.Diff
+
+	// For dev/debug
+	log.Printf("[DEBUG] nomad job diff: %s", spew.Sdump(resp.Diff))
+
+	for _, fd := range nomadDiff.Fields {
+		switch fd.Name {
+		case "Region":
+			d.SetNew("region", fd.New)
+		}
+	}
+	for _, od := range nomadDiff.Objects {
+		switch od.Name {
+		case "Datacenters":
+			dcs := d.Get("datacenters").([]interface{})
+			for _, fd := range od.Fields {
+				switch fd.Type {
+				case "Added":
+					dcs = append(dcs, fd.New)
+				case "Deleted":
+					for i := range dcs {
+						if dcs[i].(string) == fd.Old {
+							// Shift up everything after this in the slice
+							// to close the gap
+							copy(dcs[i:], dcs[i+1:])
+							dcs = dcs[:len(dcs)-1]
+							break
+						}
+					}
+				}
+			}
+			d.SetNew("datacenters", dcs)
+		}
+	}
+
+	tgMaps := d.Get("task_groups").([]interface{})
+	for _, tg := range nomadDiff.TaskGroups {
+		var tgMap map[string]interface{}
+		for _, tgmI := range tgMaps {
+			tgm := tgmI.(map[string]interface{})
+			if tgm["name"] == tg.Name {
+				tgMap = tgm
+			}
+		}
+		if tgMap == nil {
+			tgMap = map[string]interface{}{
+				"name": tg.Name,
+			}
+			tgMaps = append(tgMaps, tgMap)
+		}
+
+		for _, fd := range tg.Fields {
+			switch fd.Name {
+			case "Count":
+				num, _ := strconv.Atoi(fd.New)
+				tgMap["count"] = num
+			}
+		}
+
+		// FIXME: Also need to take care of task diffs
+	}
+	d.SetNew("task_groups", tgMaps)
+
+	return nil
 }
 
 func resourceJobRegister(d *schema.ResourceData, meta interface{}) error {
@@ -52,60 +248,35 @@ func resourceJobRegister(d *schema.ResourceData, meta interface{}) error {
 
 	// Get the jobspec itself
 	jobspecRaw := d.Get("jobspec").(string)
-
-	// Parse it
-	jobspecStruct, err := jobspec.Parse(strings.NewReader(jobspecRaw))
+	jobspecAPI, err := prepareJobSpec(jobspecRaw)
 	if err != nil {
-		return fmt.Errorf("error parsing jobspec: %s", err)
+		return err
 	}
 
-	// Initialize and validate
-	jobspecStruct.Canonicalize()
-	if err := jobspecStruct.Validate(); err != nil {
-		return fmt.Errorf("Error validating job: %v", err)
-	}
-
-	// If we have an ID and its not equal to this jobspec, then we
-	// have to deregister the old job before we register the new job.
-	prevId := d.Id()
-	if !d.Get("deregister_on_id_change").(bool) {
-		// If we aren't deregistering on ID change, just pretend we
-		// don't have a prior ID.
-		prevId = ""
-	}
-	if prevId != "" && prevId != jobspecStruct.ID {
-		log.Printf(
-			"[INFO] Deregistering %q before registering %q",
-			prevId, jobspecStruct.ID)
-
-		log.Printf("[DEBUG] Deregistering job: %q", prevId)
-		_, _, err := client.Jobs().Deregister(prevId, nil)
+	oldModifyIndexI, _ := d.GetChange("job_modify_index")
+	oldModifyIndexS := oldModifyIndexI.(string)
+	var oldModifyIndex uint64
+	if oldModifyIndexS == "" {
+		oldModifyIndex = 0
+	} else {
+		var err error
+		oldModifyIndex, err = strconv.ParseUint(oldModifyIndexS, 10, 64)
 		if err != nil {
-			return fmt.Errorf(
-				"error deregistering previous job %q "+
-					"before registering new job %q: %s",
-				prevId, jobspecStruct.ID, err)
+			// should never happen unless the plan was tampered with
+			return fmt.Errorf("invalid old job_modify_index: %s", err)
 		}
-
-		// Success! Clear our state.
-		d.SetId("")
-	}
-
-	// Convert it so that we can use it with the API
-	jobspecAPI, err := convertStructJob(jobspecStruct)
-	if err != nil {
-		return fmt.Errorf("error converting jobspec: %s", err)
 	}
 
 	// Register the job
-	_, _, err = client.Jobs().Register(jobspecAPI, nil)
+	evalID, _, err := client.Jobs().EnforceRegister(jobspecAPI, oldModifyIndex, nil)
 	if err != nil {
 		return fmt.Errorf("error applying jobspec: %s", err)
 	}
 
 	d.SetId(jobspecAPI.ID)
+	d.Set("evaluation_id", evalID)
 
-	return nil
+	return resourceJobRead(d, meta)
 }
 
 func resourceJobDeregister(d *schema.ResourceData, meta interface{}) error {
@@ -129,29 +300,83 @@ func resourceJobDeregister(d *schema.ResourceData, meta interface{}) error {
 	return nil
 }
 
-func resourceJobExists(d *schema.ResourceData, meta interface{}) (bool, error) {
+func resourceJobRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*api.Client)
 
 	id := d.Id()
-	log.Printf("[DEBUG] Checking if job exists: %q", id)
-	_, _, err := client.Jobs().Info(id, nil)
+	log.Printf("[DEBUG] Reading job %q", id)
+	job, _, err := client.Jobs().Info(id, nil)
 	if err != nil {
 		// As of Nomad 0.4.1, the API client returns an error for 404
 		// rather than a nil result, so we must check this way.
 		if strings.Contains(err.Error(), "404") {
-			return false, nil
+			log.Printf("[DEBUG] job %q no longer exists", id)
+			return nil
 		}
 
-		return true, fmt.Errorf("error checking for job: %#v", err)
+		return fmt.Errorf("error checking for job: %#v", err)
 	}
 
-	return true, nil
+	log.Printf("[DEBUG] Job from Nomad API: %s", spew.Sdump(job))
+
+	jmi := job.JobModifyIndex
+
+	d.Set("name", job.Name)
+	d.Set("job_modify_index", strconv.FormatUint(jmi, 10))
+	d.Set("region", job.Region)
+	d.Set("priority", job.Priority)
+	d.Set("datacenters", job.Datacenters)
+
+	tgMaps := make([]interface{}, len(job.TaskGroups))
+	for i, tg := range job.TaskGroups {
+		tgMap := map[string]interface{}{
+			"name":  tg.Name,
+			"count": tg.Count,
+			"meta":  tg.Meta,
+		}
+
+		tMaps := make([]interface{}, len(tg.Tasks))
+		for j, t := range tg.Tasks {
+			configJSON, _ := json.Marshal(t.Config)
+
+			tMap := map[string]interface{}{
+				"name":        t.Name,
+				"driver":      t.Driver,
+				"config_json": string(configJSON),
+			}
+			tMaps[j] = tMap
+		}
+
+		tgMap["tasks"] = tMaps
+		tgMaps[i] = tgMap
+	}
+	d.Set("task_groups", tgMaps)
+
+	return nil
 }
 
-func resourceJobRead(d *schema.ResourceData, meta interface{}) error {
-	// We don't do anything at the moment. Exists is used to
-	// remove non-existent jobs but read doesn't have to do anything.
-	return nil
+// prepareJobSpec attempts to turn a job specification string (HCL syntax)
+// into a job object ready to submit to the Nomad API.
+func prepareJobSpec(raw string) (*api.Job, error) {
+	// Parse it
+	jobspecStruct, err := jobspec.Parse(strings.NewReader(raw))
+	if err != nil {
+		return nil, fmt.Errorf("error parsing jobspec: %s", err)
+	}
+
+	// Initialize and validate
+	jobspecStruct.Canonicalize()
+	if err := jobspecStruct.Validate(); err != nil {
+		return nil, fmt.Errorf("Error validating job: %v", err)
+	}
+
+	// Convert it so that we can use it with the API
+	jobspecAPI, err := convertStructJob(jobspecStruct)
+	if err != nil {
+		return nil, fmt.Errorf("error converting jobspec: %s", err)
+	}
+
+	return jobspecAPI, nil
 }
 
 // convertStructJob is used to take a *structs.Job and convert it to an *api.Job.
